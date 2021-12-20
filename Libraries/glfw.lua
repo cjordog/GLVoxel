63631310dc5b20f1409af113052c3342d8458313
CJ Ordog <cjordog@gmail.com>
2021-12-20 13:39:10 -0800

    adding glm and imgui as submodules

--- Libraries/glfw.lua
+++ Libraries/glfw.lua
@@ -12,15 +12,33 @@ project "GLFW"
 
 	files
 	{
-		-- "glfw/include/GLFW/glfw3.h",
-		-- "glfw/include/GLFW/glfw3native.h",
-		"glfw/src/glfw_config.h",
+		"glfw/include/GLFW/glfw3.h",
+		"glfw/include/GLFW/glfw3native.h",
+		-- "glfw/src/glfw_config.h",
+		-- "glfw/src/context.c",
+		-- "glfw/src/init.c",
+		-- "glfw/src/input.c",
+		-- "glfw/src/monitor.c",
+		-- "glfw/src/vulkan.c",
+		-- "glfw/src/window.c"
+		"glfw/src/internal.h",
+		"glfw/src/platform.h",
+		"glfw/src/mappings.h",
 		"glfw/src/context.c",
 		"glfw/src/init.c",
 		"glfw/src/input.c",
 		"glfw/src/monitor.c",
+		"glfw/src/platform.c",
 		"glfw/src/vulkan.c",
-		"glfw/src/window.c"
+		"glfw/src/window.c",
+		"glfw/src/egl_context.c",
+		"glfw/src/osmesa_context.c",
+		"glfw/src/null_platform.h",
+		"glfw/src/null_joystick.h",
+		"glfw/src/null_init.c",
+		"glfw/src/null_monitor.c",
+		"glfw/src/null_window.c",
+		"glfw/src/null_joystick.c",
 	}
     
 	filter "system:linux"
@@ -54,15 +72,27 @@ project "GLFW"
 
 		files
 		{
-			"glfw/src/win32_init.c",
-			"glfw/src/win32_joystick.c",
-			"glfw/src/win32_monitor.c",
-			"glfw/src/win32_time.c",
-			"glfw/src/win32_thread.c",
-			"glfw/src/win32_window.c",
-			"glfw/src/wgl_context.c",
-			"glfw/src/egl_context.c",
-			"glfw/src/osmesa_context.c"
+			-- "glfw/src/win32_init.c",
+			-- "glfw/src/win32_joystick.c",
+			-- "glfw/src/win32_monitor.c",
+			-- "glfw/src/win32_time.c",
+			-- "glfw/src/win32_thread.c",
+			-- "glfw/src/win32_window.c",
+			-- "glfw/src/wgl_context.c",
+			-- "glfw/src/egl_context.c",
+			-- "glfw/src/osmesa_context.c"
+			"glfw/src/win32_time.h",
+			"glfw/src/win32_thread.h",
+			"glfw/src/win32_module.c",
+            "glfw/src/win32_time.c",
+            "glfw/src/win32_thread.c",
+            "glfw/src/win32_platform.h",
+            "glfw/src/win32_joystick.h",
+            "glfw/src/win32_init.c",
+            "glfw/src/win32_joystick.c",
+            "glfw/src/win32_monitor.c",
+            "glfw/src/win32_window.c",
+            "glfw/src/wgl_context.c",
 		}
 
 		defines 
